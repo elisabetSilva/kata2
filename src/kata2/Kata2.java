1945f2ff34aab5e259267f4d78e6b362d540ebb6
elisabetSilva <43438513+elisabetSilva@users.noreply.github.com>
2018-11-12 21:16:27 +0000

    Kata 2 versión 3

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -1,21 +1,15 @@
 package kata2;
 
-import java.util.HashMap;
 import java.util.Map;
 
 public class Kata2 {
     public static void main(String[] args) {
         int[] data = new int[] {1,1,1,100,2,4,5,6,8,8,4,5,6,2,-4};
-        Map<Integer, Integer> histogram = new HashMap<>();
+        Histogram histo = new Histogram(data);
+        Map<Integer,Integer> histogr = histo.getHistogram();
         
-        //Se asigana para cada clave, un valor que representa el número de veces que se repite
-        for (int value : data) {
-            histogram.put(value, histogram.containsKey(value) ? histogram.get(value)+1 : 1);
+        for (int key : histogr.keySet()) {
+           System.out.println(key + "==>" + histogr.get(key)); 
         }
-        
-       //Recorre el conjunto de claves y muestra para cada clave su valor
-       for (Integer key : histogram.keySet()) {
-           System.out.println(key + " ==> " + histogram.get(key));
-       }
-    }  
-} 
+    }
+}
